2f70cdd0232199072635a4720455168adb73de09
sheemungai <mungaisheilla19@gmail.com>
2025-12-04 23:33:26 +0300

    create and read one operations

--- projectAlpha/firstApp/models.py
+++ projectAlpha/firstApp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title= models.CharField(max_length=100)
+    description= models.CharField(max_length=255)
+    category= models.CharField(max_length=200)
+    tutor= models.CharField(max_length=100)
+    image = models.CharField(max_length=255)
+    price= models.DecimalField(max_digits=10, decimal_places=2)
+    created_at= models.DateField(auto_now_add=True)
+    updated_at= models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons') 
+    title= models.CharField(max_length=100)
+    description= models.CharField(max_length=255)
+    video_url = models.URLField(blank=True)  # YouTube/Vimeo link OR
+    video_file = models.FileField(upload_to='lesson_videos/', blank=True)  # Upload video
+    pdf = models.FileField(upload_to='lesson_pdfs/', blank=True)
+    duration = models.CharField(max_length=50, blank=True)
+    created_at= models.DateField(auto_now_add=True)
+    updated_at= models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+    
+
+class Subscription(models.Model):
+    PLAN_TYPES = [
+        ('free', 'Free'),
+        ('monthly', 'Monthly'),
+        ('yearly', 'Yearly'),
+        ('lifetime', 'Lifetime'),
+    ]
+    name = models.CharField(max_length=100)
+    plan_type = models.CharField(max_length=20, choices=PLAN_TYPES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
--- projectAlpha/firstApp/views.py
+++ projectAlpha/firstApp/views.py
@@ -1,6 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import CourseForm, LessonForm, SubscriptionForm
+from . models import Course, Lesson, Subscription
 
 # Create your views here.
 def home(request):
     context={}
     return render(request, 'firstApp/home.html', context)
+
+
+def createCourse(request):
+    form = CourseForm()
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form' : form}
+    return render(request,'firstApp/form.html' ,context)
+
+def createLesson(request):
+    form = LessonForm()
+
+    if request.method == 'POST':
+        form = LessonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form' : form}
+    return render(request,'firstApp/form.html' ,context)
+
+
+def createSubscription(request):
+
+    form = SubscriptionForm()
+
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form' : form}
+    return render(request,'firstApp/form.html' ,context)
+
+# readall
+def readCourses(request):
+    courses = Course.objects.all()
+    context ={'data':courses}
+    return render(request, 'firstApp/home.html', context)
+
+def readLessons(request):
+    lessons = Lesson.objects.all()
+    context ={'data':lessons}
+    return render(request, 'firstApp/home.html', context)
+
+def readSubscriptions(request):
+    subscriptions = Subscription.objects.all()
+    context ={'data':subscriptions}
+    return render(request, 'firstApp/home.html', context)
+
+# readone
+def readOneCourse(request, pk):
+    course = Course.objects.get(id=pk)
+    context ={'item':course}
+    return render(request, 'firstApp/item.html', context)
+
+def readOneLesson(request, pk):
+    lesson = Lesson.objects.get(id=pk)
+    context ={'item':lesson}
+    return render(request, 'firstApp/item.html', context)
+
+def readOneSubscription(request, pk):
+    subscription = Subscription.objects.get(id=pk)
+    context ={'item':subscription}
+    return render(request, 'firstApp/item.html', context)
